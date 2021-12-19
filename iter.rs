a4b22c7ad19c57641c00259ccfe6b02dc2d6a647
Pranav543 <pranavnaik543@gmail.com>
2021-12-19 21:55:26 +0530

    second_commit

---
+++ iter.rs
@@ -0,0 +1,24 @@
+// Won't run
+
+// fn main() {
+//     let arr = [10, 20, 30];
+//     for i in arr {
+//         println!("{}", i);
+//     }
+// }
+
+// Will Run
+
+fn main() {
+    let arr = [10, 20, 30];
+    for i in arr.iter() {
+        println!("{}", i);
+    }
+
+    // slices will be converted implicitly to iterators...
+    let slice = &arr;
+    for i in slice {
+        println!("{}", i);
+    }
+}
+
