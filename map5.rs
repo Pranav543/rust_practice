a33a86ac323b2de79389b6ad49afdfabdb68b6fd
Pranav543 <pranavnaik543@gmail.com>
2022-01-17 00:21:04 +0530

    trait_binary_heap

---
+++ map5.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+fn main() {
+    let data = vec![ // This is the raw data
+        ("male", 9),
+        ("female", 5),
+        ("male", 0),
+        ("female", 6),
+        ("female", 5),
+        ("male", 10),
+    ];
+
+    let mut survey_hash = HashMap::new();
+
+    for item in data { // This gives a tuple of (&str, i32)
+        survey_hash.entry(item.0).or_insert(Vec::new()).push(item.1); // This pushes the number into the Vec inside
+    }
+
+    for (male_or_female, numbers) in survey_hash {
+        println!("{:?}: {:?}", male_or_female, numbers);
+    }
+}
---
+++ trait2.rs
@@ -0,0 +1,27 @@
+struct Animal { // A simple struct - an Animal only has a name
+    name: String,
+}
+
+trait Dog { // The dog trait gives some functionality
+    fn bark(&self) { // It can bark
+        println!("Woof woof!");
+    }
+    fn run(&self) { // and it can run
+        println!("The dog is running!");
+    }
+}
+
+impl Dog for Animal {
+    fn run(&self) {
+        println!("{} is running!", self.name);
+    }
+}
+
+fn main() {
+    let rover = Animal {
+        name: "Rover".to_string(),
+    };
+
+    rover.bark(); // Now Animal can use bark()
+    rover.run();  // and it can use run()
+}
