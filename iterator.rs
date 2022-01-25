3448258c80623ce8f33d7a95264857220fd6c9c0
Pranav543 <pranavnaik543@gmail.com>
2022-01-25 23:51:53 +0530

    iterator

--- iterator.rs
+++ iterator.rs
@@ -1,35 +1,48 @@
-#[derive(Debug)] // we want to print it with {:?}
+#[derive(Debug, Clone)]
 struct Library {
-    library_type: LibraryType, // this is our enum
-    books: Vec<String>, // list of books
+    library_type: LibraryType,
+    books: Vec<String>,
 }
 
-#[derive(Debug)]
-enum LibraryType { // libraries can be city libraries or country libraries
+#[derive(Debug, Clone)]
+enum LibraryType {
     City,
     Country,
 }
 
 impl Library {
-    fn add_book(&mut self, book: &str) { // we use add_book to add new books
-        self.books.push(book.to_string()); // we take a &str and turn it into a String, then add it to the Vec
+    fn add_book(&mut self, book: &str) {
+        self.books.push(book.to_string());
     }
 
-    fn new() -> Self { // this creates a new Library
+    fn new() -> Self {
         Self {
-            library_type: LibraryType::City, // most are in the city so we'll choose City
-                                             // most of the time
+            library_type: LibraryType::City,
+            // most of the time
             books: Vec::new(),
         }
     }
 }
 
+impl Iterator for Library {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        match self.books.pop() {
+            Some(book) => Some(book + " is found!"), // Rust allows String + &str
+            None => None,
+        }
+    }
+}
+
 fn main() {
-    let mut my_library = Library::new(); // make a new library
-    my_library.add_book("The Doom of the Darksword"); // add some books
+    let mut my_library = Library::new();
+    my_library.add_book("The Doom of the Darksword");
     my_library.add_book("Demian - die Geschichte einer Jugend");
     my_library.add_book("구운몽");
     my_library.add_book("吾輩は猫である");
 
-    println!("{:?}", my_library.books); // we can print our list of books
+    for item in my_library.clone() { // we can use a for loop now. Give it a clone so Library won't be destroyed
+        println!("{}", item);
+    }
 }
